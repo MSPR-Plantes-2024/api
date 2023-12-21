382e71daef178f23750eb841163df3a304903071
Thibaud-LAMON <thib.lamon@gmail.com>
2023-12-21 15:11:10 +0100

    Creating Get request in UserController

--- arosaje/src/main/java/com/mspprarosaje/arosaje/api/UserController.java
+++ arosaje/src/main/java/com/mspprarosaje/arosaje/api/UserController.java
@@ -1,5 +1,25 @@
 package com.mspprarosaje.arosaje.api;
 
+import com.mspprarosaje.arosaje.api.dto.UserMinimalDTO;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/users")
 public class UserController {
 
+    @GetMapping
+    public ResponseEntity<List<UserMinimalDTO>> getUsers(){
+        UserMinimalDTO userMinimalDTO = UserMinimalDTO
+                .builder()
+                .id(1)
+                .firstName("Louis")
+                .lastName("Dupont")
+                .build();
+        return ResponseEntity.ok(List.of(userMinimalDTO));
+    }
 }
